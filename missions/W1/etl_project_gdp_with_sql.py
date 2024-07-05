ca94cb5e1a25747abad1a297b8ec54488e943a7c
jang-namu <ironhee8005@gmail.com>
2024-07-05 15:50:03 +0900

    chore: type hint, comment 재배치

--- missions/W1/etl_project_gdp_with_sql.py
+++ missions/W1/etl_project_gdp_with_sql.py
@@ -25,7 +25,7 @@ def create_table():
         conn.close()
 
 
-def exec_query(query):
+def exec_query(query: str) -> pd.DataFrame:
     try:
         with sqlite3.connect(db_name) as conn:
             result = pd.read_sql(query, conn)
@@ -36,10 +36,10 @@ def exec_query(query):
     return result
 
 
-"""
-완성된 데이터프레임을 받아 target 데이터베이스 {table_name} 테이블에 행으로 삽입한다.
-"""
-def load(df):
+def load(df: pd.DataFrame):
+    """
+    완성된 데이터프레임을 받아 target 데이터베이스 {table_name} 테이블에 행으로 삽입한다.
+    """
     log_info("Starting Load Task: Initiating data loading into the target system.")
     try:
         with sqlite3.connect(db_name) as conn:
@@ -51,11 +51,11 @@ def load(df):
     log_info("Completed Load Task: Data loading into the target system completed successfully.")
 
 
-"""
-로깅을 위한 logger를 초기화하여 반환한다.
-로그를 File에 출력하기 위한 핸들러를 추가하고 로그 레벨을 INFO로 설정한다. 
-"""
-def init_logger(log_file):
+def init_logger(log_file: str) -> logging.Logger:
+    """
+    로깅을 위한 logger를 초기화하여 반환한다.
+    로그를 File에 출력하기 위한 핸들러를 추가하고 로그 레벨을 INFO로 설정한다.
+    """
     global logger
     logger = logging.getLogger(__name__)
     # init_logger를 여러번 호출하는 경우 싱글톤 패턴을 따르는 loggin.getLogger에 같은 핸들러를 여러번 추가하여 하나의 로그가 여러번 찍히는 것을 방지
@@ -66,19 +66,19 @@ def init_logger(log_file):
     return logger
 
 
-"""
-'[Year-Monthname-Day-Hour-Minute-Second], <message>' 형식으로 로그를 출력한다.
-"""
-def log_info(msg):
+def log_info(msg: str):
+    """
+    '[Year-Monthname-Day-Hour-Minute-Second], <message>' 형식으로 로그를 출력한다.
+    """
     now = datetime.now()  # 현재 날짜와 시간 가져오기
     formatted_date = now.strftime('[%Y-%B-%d-%H-%M-%S], ')  # 원하는 포맷으로 변환
     logger.info(formatted_date + msg)
 
 
-"""
-'https://restcountries.com/v3.1/all' API를 호출하고 받아온 데이터를 파싱하여 ['Region', 'Country'] 컬럼을 갖는 데이터프레임을 반환한다.
-"""
-def read_continents():
+def read_continents() -> pd.DataFrame:
+    """
+    'https://restcountries.com/v3.1/all' API를 호출하고 받아온 데이터를 파싱하여 ['Region', 'Country'] 컬럼을 갖는 데이터프레임을 반환한다.
+    """
     response = requests.get('https://restcountries.com/v3.1/all')
     data = response.json()
     countries = []
@@ -89,18 +89,18 @@ def read_continents():
     return pd.DataFrame(countries)
 
 
-"""
-bs4 라이브러리를 통해 위키피디아 페이지를 스크래핑, 국가별 GDP 테이블을 가져온다.
-테이블에서 국가 이름과 IMF에서 산정한 GDP(단위: Million) 쌍을 하나씩 읽어 data에 담아 반환한다.
+def extract() -> list:
+    """
+    bs4 라이브러리를 통해 위키피디아 페이지를 스크래핑, 국가별 GDP 테이블을 가져온다.
+    테이블에서 국가 이름과 IMF에서 산정한 GDP(단위: Million) 쌍을 하나씩 읽어 data에 담아 반환한다.
 
-예)
-    ['United States', '28,781,083']
-    ['China', '18,532,633']
-    ['Germany', '4,591,100']
-    ['Japan', '4,110,452']
-             ...
-"""
-def extract():
+    예)
+        ['United States', '28,781,083']
+        ['China', '18,532,633']
+        ['Germany', '4,591,100']
+        ['Japan', '4,110,452']
+                 ...
+    """
     log_info("Starting Extract Task: Initiating data extraction from the source.")
     data = []
     response = requests.get(url)
@@ -123,11 +123,11 @@ def extract():
     return data
 
 
-"""
-extract 과정에서 뽑운 데이터를 통해 Country-GDP_USD_billion의 데이터프레임을 만들어 반환한다.
-GDP의 내림차순으로 정렬하며, Million 단위의 값을 Billion(소수 둘째자리)로 변환한다. 
-"""
-def transform(data):
+def transform(data: list) -> pd.DataFrame:
+    """
+    extract 과정에서 뽑운 데이터를 통해 Country-GDP_USD_billion의 데이터프레임을 만들어 반환한다.
+    GDP의 내림차순으로 정렬하며, Million 단위의 값을 Billion(소수 둘째자리)로 변환한다.
+    """
     log_info("Starting Transform Task: Initiating data transformation process.")
 
     df = pd.DataFrame(data, columns=['Country', 'GDP'])
@@ -139,30 +139,31 @@ def transform(data):
     return result
 
 
-"""
-GDP(Billion Dollar)가 condition 이상인 항목을 조회한다.
-"""
-def show_over(condition):
+def show_over(condition: int):
+    """
+    GDP(Billion Dollar)가 condition 이상인 항목을 조회한다.
+    """
     query = f'SELECT * FROM {table_name} WHERE GDP_USD_billion >= {condition}'
     df = exec_query(query)
     df.reindex(columns=['index', 'Country', 'GDP_USD_billion', 'Region'])
     print(df[df[df.columns[2]] >= condition].set_index('Country'))
 
 
-"""
-country-gdp로 구성된 데이터프레임과 region-country로 구성된 데이터프레임을 병합해 country-gdp-region 데이터프레임을 만든다.
-어떤 region에도 속하지 않는 country의 region은 'N/A'로 채우고, 어떤 country도 속하지 않는 region은 결과에 포함하지 않는다.
-"""
-def merge(country_gdp, region_country):
+def merge(country_gdp: pd.DataFrame, region_country: pd.DataFrame) -> pd.DataFrame:
+    """
+    country-gdp로 구성된 데이터프레임과 region-country로 구성된 데이터프레임을 병합해 country-gdp-region 데이터프레임을 만든다.
+    어떤 region에도 속하지 않는 country의 region은 'N/A'로 채우고, 어떤 country도 속하지 않는 region은 결과에 포함하지 않는다.
+    """
     df_merged = pd.merge(country_gdp, region_country, on='Country', how='left')
     df_merged['Region'] = df_merged['Region'].fillna('N/A')
     return df_merged
 
 
-"""
-['Region', 'AVG_GDP_USD_billion'] 조회 'Region'을 기준으로 그룹화하고, 개별 그룹에서 GDP 상위 n개 항목의 평균을 조회한다. 
-"""
-def show_topn_mean_region(n):
+
+def show_topn_mean_region(n: int):
+    """
+    ['Region', 'AVG_GDP_USD_billion'] 조회 'Region'을 기준으로 그룹화하고, 개별 그룹에서 GDP 상위 n개 항목의 평균을 조회한다.
+    """
     query = f"""
                 SELECT Region, ROUND(AVG(GDP_USD_billion), 2) AS AVG_GDP_USD_billion
                 FROM (
@@ -180,7 +181,7 @@ def show_topn_mean_region(n):
     print(result)
 
 
-def execute(n, condition):
+def execute(n: int, condition: int):
     # 테이블 생성
     create_table()
     # 로거 초기화
